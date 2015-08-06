5f9eb61920d8b6a9a883c7a49139700f49f06edf
Josh Rose <gamahead@gmail.com>
2015-08-06 11:53:08 -0400

    Got a TP saccading around a number

--- num_to_sdr_old.py
+++ num_to_sdr_old.py
@@ -42,6 +42,10 @@ def spatioretinal_encoder(encoding, x, y):
 
 	location = location_matrix.flatten().tolist()
 
+	print_num(retina, retina_radius, inactive_char = "-")
+	print_num(location, inactive_char = "-")
+
+
 	return(retina,location)
 
 def generate_patches(patch_width, retina):
@@ -82,41 +86,53 @@ class Column:
 		self.predicted = 0
 		self.predicted_w_strength = 0
 
-def saccade(x,y):
-	""" Generate a saccade from position x,y """
-	new_x = x + r.choice(range(-2,3))
-	new_y = y + r.choice(range(-2,3))
-	while new_x not in range(left_buffer, image_width - right_buffer):
-		new_x = x + r.choice(range(-2,3))
-	while new_y not in range(left_buffer, image_width - right_buffer):
-		new_y = y + r.choice(range(-2,3))
-	return(new_x,new_y)
 
-def get_indices(vector):
-	inds = []
-	for i in range(len(vector)):
-		if vector[i] == 1:
-			inds.append(i)
-	return(inds)
-
-
-# =============================================
-# Construct Nums
-# =============================================
-answers = []
-num_data = []
 with open('train.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        if row_num == 1:
+        if row_num == 2:
         	first_num_line = ['1' if int(x) >= 1 else '0' for x in row]
 
-        if row_num > 0:
-        	num_data.append(['1' if int(x) >= 1 else '0' for x in row])
-        	answers.append(num_data[-1].pop(0))
         row_num = row_num + 1
+
     first_num_name = first_num_line.pop(0)
     # generate_picture(first_num_line)
+
+print_num(first_num_line)
+# print first_num_line
+# generate_picture([float(x) for x in first_num_line])
+saccades = [(r.randrange(left_buffer, image_width - right_buffer), r.randrange(left_buffer, image_width - right_buffer)) for x in range(10000)]
+(retina,location) = spatioretinal_encoder(first_num_line,saccades[0][0],saccades[0][1])
+# columns = []
+# patches = generate_patches(3,retina)
+# print('**** patches generated ****')
+# for patch in patches:
+# 	# print(patch)
+# 	columns.append(Column( [Synapse(s) for s in patch]))
+# print('**** columns built ****')
+# print('col_num = ',len(columns))
+
+# with open('columns.p', 'w') as outfile:
+# 	pickle.dump(columns,outfile)
+# print('dumped')
+# print('loading')
+with open('columns.p', 'r') as infile:
+	columns = pickle.load( open( "columns.p", "rb" ) )
+
+print_num(retina, 10, inactive_char = '-')
+
+retina = np.array([int(i) for i in retina]).reshape(10,10)
+active_columns = [ c for c in columns if sum([ retina[s.source_input[0]][s.source_input[1]] for s in c.connected_synapses] ) in range(2,8)]
+print_str = ''
+for c in columns:
+	if c in active_columns:
+		print_str += '1'
+	else:
+		print_str += '0'
+
+col_str = np.array([int(x) for x in print_str]).reshape(8,8)
+print(col_str)
+print(sum(sum(col_str)))
 
 from nupic.research.TP10X2 import TP10X2 as TP
 
@@ -133,23 +149,15 @@ def formatRow(x):
 
 
 # Step 1: create Temporal Pooler instance with appropriate parameters
-tp = TP(numberOfCols=50, cellsPerColumn=4,
+tp = TP(numberOfCols=50, cellsPerColumn=2,
                 initialPerm=0.5, connectedPerm=0.5,
                 minThreshold=10, newSynapseCount=10,
-                permanenceInc=0.1, permanenceDec=0.1,
+                permanenceInc=0.1, permanenceDec=0.0,
                 activationThreshold=8,
                 globalDecay=0, burnIn=1,
                 checkSynapseConsistency=False,
                 pamLength=10)
 
-ret_tp = TP(numberOfCols=848, cellsPerColumn=32,
-                initialPerm=0.5, connectedPerm=0.5,
-                minThreshold=10, newSynapseCount=10,
-                permanenceInc=0.01, permanenceDec=0.02,
-                activationThreshold=8,
-                globalDecay=0, burnIn=1,
-                checkSynapseConsistency=False,
-                pamLength=10)
 
 
 # Step 2: create input vectors to feed to the temporal pooler. Each input vector
@@ -171,6 +179,7 @@ for i in range(0,1000):
 	else:
 		iput = input2
 
+print_num([str(x) for x in input1.flatten()], n=5, inactive_char='-')
 # Step 3: send this simple sequence to the temporal pooler for learning
 # We repeat the sequence 10 times
 for i in range(10):
@@ -181,8 +190,6 @@ for i in range(10):
   		iput = input1
   	else:
   		iput = input2
-
-  	# Testing the TP's ability to handle my custom input
   	tp.compute(iput, enableLearn = True, computeInfOutput = False)
 
 
@@ -190,20 +197,19 @@ for i in range(10):
   # zeros out all the states. It is not strictly necessary but it's a bit
   # messier without resets, and the TP learns quicker with resets.
   tp.reset()
-
-
+  
 
 #######################################################################
 #
 # Step 3: send the same sequence of vectors and look at predictions made by
 # temporal pooler
 for j in range(5):
-  # print "\n\n--------","ABCDE"[j],"-----------"
+  print "\n\n--------","ABCDE"[j],"-----------"
   if j % 2 == 0:
   	iput = input1
   else:
   	iput = input2
-  # print "Raw input vector\n",formatRow(iput)
+  print "Raw input vector\n",formatRow(iput)
   
   # Send each vector to the TP, with learning turned off
   tp.compute(iput, enableLearn = False, computeInfOutput = True)
@@ -216,18 +222,18 @@ for j in range(5):
   # What you should notice is that the columns where active state is 1
   # represent the SDR for the current input pattern and the columns where
   # predicted state is 1 represent the SDR for the next expected pattern
-  # print "\nAll the active and predicted cells:"
-  # tp.printStates(printPrevious = False, printLearnState = False)
+  print "\nAll the active and predicted cells:"
+  tp.printStates(printPrevious = False, printLearnState = False)
   
   # tp.getPredictedState() gets the predicted cells.
   # predictedCells[c][i] represents the state of the i'th cell in the c'th
   # column. To see if a column is predicted, we can simply take the OR
   # across all the cells in that column. In numpy we can do this by taking 
   # the max along axis 1.
-  # print "\n\nThe following columns are predicted by the temporal pooler. This"
-  # print "should correspond to columns in the *next* item in the sequence."
+  print "\n\nThe following columns are predicted by the temporal pooler. This"
+  print "should correspond to columns in the *next* item in the sequence."
   predictedCells = tp.getPredictedState()
-  # print formatRow(predictedCells.max(axis=1).nonzero())
+  print formatRow(predictedCells.max(axis=1).nonzero())
 
 input1 = np.array([1]*10 + [0]*15).reshape((5,5))
 input2 = np.array([0]*15 + [1]*10).reshape((5,5))
@@ -237,88 +243,17 @@ for i in range(0,1000):
 	else:
 		iput = input2
 
-# =============================================
-# Construct Encodings
-# =============================================
-saccades = [(r.randrange(left_buffer, image_width - right_buffer), r.randrange(left_buffer, image_width - right_buffer)) for x in range(1)]
-retinas = []
-locations = []
-final_encodings = []
-for i in range(500):
-	saccades.append(saccade(saccades[i][0],saccades[i][1]))
-for sac in saccades:
-	(retina,location) = spatioretinal_encoder(first_num_line,sac[0],sac[1])
-	# print_num(retina, 10, inactive_char='-')
-	retinas.append(retina)
-	locations.append(location)
-
-for i in range(len(retinas)):
-	final_encodings.append([int(re) for re in retinas[i]]+[int(lo) for lo in locations[i]])
-
-# =============================================
-# Grab 'inhibition columns'
-# These are connected to 3x3 subgrids of the retina
-# =============================================
-with open('columns.p', 'r') as infile:
-	columns = pickle.load( open( "columns.p", "rb" ) )
+print_num([str(x) for x in input1.flatten()], n=5, inactive_char='-')
+# with open('1_100.csv', 'wb') as csvfile:
+#     spamwriter = csv.writer(csvfile, delimiter=',')
+#     counter = 1
+#     for saccade in saccades:
+#     	if counter <= 100: 
+#     		spamwriter.writerow(spatioretinal_encoder(first_num_line,saccade[0],saccade[1]))
+#     		counter += 1
 
 
 
-retina = np.array([int(i) for i in retina]).reshape(10,10)
-active_columns = [ c for c in columns if sum([ retina[s.source_input[0]][s.source_input[1]] for s in c.connected_synapses] ) in range(2,8)]
-print_str = ''
-for c in columns:
-	if c in active_columns:
-		print_str += '1'
-	else:
-		print_str += '0'
 
-print_num(print_str,n=8,inactive_char='-')
-col_str = np.array([int(x) for x in print_str]).reshape(8,8)
-print(col_str)
-print(sum(sum(col_str)))	
-recent_pred_cells = [[],[],[]]
-
-for i in range(3):
-	datum = final_encodings[i]
-	iput = np.array(datum, dtype="uint32")
-	ret_tp.compute(iput, enableLearn = True, computeInfOutput = True)
-	recent_pred_cells[i%3] = ret_tp.getPredictedState().flatten()
-
-inds = get_indices([item for sublist in recent_pred_cells for item in sublist])
-cell_activation_dict = {}
-for i in range(84864):
-	cell_activation_dict[i] = 0
-
-for i in range(len(final_encodings)):
-	datum = final_encodings[i]
-	iput = np.array(datum, dtype="uint32")
-	ret_tp.compute(iput, enableLearn = True, computeInfOutput = True)
-
-	predictedCells = ret_tp.getPredictedState()
-	recent_pred_cells[i%3] = predictedCells.flatten()
-	predicted_ret_vector = ['1' if 1 in x else '0' for x in predictedCells[0:100]]
-	predicted_loc_vector = ['1' if 1 in x else '0' for x in predictedCells[100:884]]
-
-	# print('**** RETINA ****')
-	# print_num(retinas[i], n=10, inactive_char='-')
-
-	# print('**** LOC ****')
-	# print_num(locations[i], inactive_char='-')
-
-	# print('**** PRED_RETINA ****')
-	# print_num(predicted_ret_vector,n=10,inactive_char='-')
-
-	# print('**** PRED_LOC ****')
-	# print_num(predicted_loc_vector,inactive_char='-')
-
-	inds = get_indices([item for sublist in recent_pred_cells for item in sublist])
-	for index in inds:
-		cell_activation_dict[index] += 1
-
-
-
-sorted_cells = sorted(cell_activation_dict.iteritems(), key=lambda (k,v): (v,k), reverse=False)
-pickle.dump(sorted_cells, open( "1Cells.p", "wb" ) )
 
 
